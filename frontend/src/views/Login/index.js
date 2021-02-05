6c4dbba0b3e2da40d42c3f69522b84698692f833
douglas-cezaro <cezarodouglas@gmail.com>
2021-02-05 00:17:56 -0300

    Configuration fonts

--- frontend/src/views/Login/index.js
+++ frontend/src/views/Login/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Text } from "react-native";
-import { Container } from "./styles";
+import { Container, ContainerImage, ContainerTitle, Title } from "./styles";
+import ImageLogin from "../../../assets/images/imageLogin.png";
 
 export default function Login() {
   return (
     <Container>
-      <Text>View Login</Text>
+      <ContainerImage source={ImageLogin}></ContainerImage>
+      <ContainerTitle>
+        <Title>Transitar</Title>
+      </ContainerTitle>
     </Container>
   );
 }
