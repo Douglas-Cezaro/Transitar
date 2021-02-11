9e850cf0d20b05425ff32ecf8defe90b750854aa
douglas-cezaro <cezarodouglas@gmail.com>
2021-02-10 23:04:47 -0300

    implemented validate token

--- frontend/src/views/Preload/index.js
+++ frontend/src/views/Preload/index.js
@@ -1,11 +1,44 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect } from "react";
 import { Container } from "./styles";
+import { AsyncStorage } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Api from "../../api/index";
+import Loading from "../../components/loading";
 
 export default function Preload() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem("token");
+      console.log(token);
+      if (token) {
+        let config = {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        };
+        console.log(config);
+        let res = await Api.get("validatetoken", config);
+        console.log(res);
+        if (res.token) {
+          await AsyncStorage.setItem("token", token);
+          navigation.reset({
+            routes: [{ name: "MainTab" }],
+          });
+        } else {
+          navigation.navigate("MainTab");
+        }
+      } else {
+        navigation.navigate("Login");
+      }
+    };
+    checkToken();
+  }, []);
+
   return (
     <Container>
-      <Text>View Preload</Text>
+      <Loading />
     </Container>
   );
 }
